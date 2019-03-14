1b31654c651af598c9a81c91ce06e668f75edf61
Alexei Daniline <alexei.daniline@traderev.com>
2019-03-14 15:48:17 -0400

    reverted the allocationId change

--- src/index.ts
+++ src/index.ts
@@ -207,7 +207,7 @@ export class Vpc extends ComponentResource implements VpcOutputs {
                 Name: `${inputs.description} NAT GW ${index + 1}`,
             }, inputs.baseTags);
             const natGateway = new aws.ec2.NatGateway(`${baseName}-nat-gw-${index + 1}`, {
-                allocationId: eip.allocationId,
+                allocationId: eip.id,
                 subnetId: publicSubnets[index].id,
                 tags: natGatewayTags,
             }, subnetParent);
